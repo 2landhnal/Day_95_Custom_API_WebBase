4b1fdaffc048507facf1aefd419c9acebc2ee2ab
2landhnal <93079233+2landhnal@users.noreply.github.com>
2022-01-29 15:12:06 +0700

    host 0.0.0.0

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def home():
    return render_template('index.html', coms=coms)
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host='0.0.0.0', port=5000)
 
 ### Sample ###
 # with open('static/data.csv', 'r') as read_obj:
